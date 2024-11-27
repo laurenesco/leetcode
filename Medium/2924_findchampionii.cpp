93cdd011d8c370bfae25e0fa80b815ac40b3ad71
lescobedo4@mail.stmarytx.edu <89123282+laurenesco@users.noreply.github.com>
2024-11-27 15:48:19 -0600

    2924 find chamption ii commented

--- Medium/2924_findchampionii.cpp
+++ Medium/2924_findchampionii.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-	int findChampion(int n, vector<vector<int>>& edges) {
-		 int winners = 0;
-		 int windex = 0;
-		 vector<int> indegrees(n, 0);
+    int findChampion(int n, vector<vector<int>>& edges) {
+        int winners = 0; // Record how many winners there are
+        int windex = 0; // Record the winners index
+        vector<int> indegrees(n, 0); // Track node indegrees
 
-		 for (int i = 0; i < edges.size(); i ++) {
-			  indegrees[edges[i][1]] ++;
-		 }
+        // Record all nodes with indegrees (losers)
+        for (int i = 0; i < edges.size(); i ++) {
+            indegrees[edges[i][1]] ++;
+        }
 
-		 for (int i = 0; i < indegrees.size(); i ++) {
-			  if (indegrees[i] == 0) {
-					winners ++;
-					windex = i;
-			  }
+        // Record which nodes have no indegrees (winners)
+        for (int i = 0; i < indegrees.size(); i ++) {
+            if (indegrees[i] == 0) {
+                winners ++;
+                windex = i;
+            }
 
-			  if (winners > 1) {
-					return -1;
-			  }
-		 }
+            // Cut short if multiple winners/tie, based on problem specification
+            if (winners > 1) {
+                return -1;
+            }
+        }
 
-		 int solution = winners == 0 ? -1 : windex;
-
-		 return solution;
-	}
+        // If we made it this far, there's only one winner
+        return windex;
+    }
 };
